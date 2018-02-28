7a8c2c622e1dcc9e0634626f2714ac9adac743cb
Glisse Lisbeth <lizbeth1986.jm@gmail.com>
2018-02-28 16:04:54 +0000

    add configuration gulp for HTML

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,18 @@
 var gulp = require('gulp');
+var pug = require('gulp-pug');
 var babel = require('gulp-babel');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
 var cleanCSS = require('gulp-clean-css');
 var del = require('del');
+var imagemin = require('gulp-imagemin');
 
 var paths = {
+  html: {
+    src: 'src/*.html',
+    dest: 'assets/'
+  },
   styles: {
     src: 'src/styles/**/*.css',
     dest: 'assets/styles/'
@@ -14,6 +20,10 @@ var paths = {
   scripts: {
     src: 'src/scripts/**/*.js',
     dest: 'assets/scripts/'
+  },
+  images: {
+    src: 'src/images/**/*.{jpg,jpeg,png}',
+    dest: 'assets/images/'
   }
 };
 
@@ -21,6 +31,11 @@ function clean() {
   return del([ 'assets' ]);
 }
 
+function html() {
+  return gulp.src(paths.html.src)
+    .pipe(pug())
+    .pipe(gulp.dest(paths.html.dest))
+}
 function styles() {
   return gulp.src(paths.styles.src)
     .pipe(cleanCSS())
@@ -39,18 +54,26 @@ function scripts() {
     .pipe(gulp.dest(paths.scripts.dest));
 }
 
+function images() {
+  return gulp.src(paths.images.src, {since: gulp.lastRun(images)})
+    .pipe(imagemin({optimizationLevel: 5}))
+    .pipe(gulp.dest(paths.images.dest));
+}
+
 function watch() {
+  gulp.watch(paths.html.src, html)
   gulp.watch(paths.scripts.src, scripts);
   gulp.watch(paths.styles.src, styles);
+  gulp.watch(paths.images.src, images);
 }
 
 exports.clean = clean;
+exports.html = html;
 exports.styles = styles;
 exports.scripts = scripts;
+exports.images = images;
 exports.watch = watch;
 
-var build = gulp.series(clean, gulp.parallel(styles, scripts));
+var build = gulp.series(clean, gulp.parallel(html, styles, scripts, images));
 
 gulp.task('build', build);
-
-gulp.task('default', build);
