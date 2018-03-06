126d477610b3f600a789e2534e6d0f939d49099b
Glisse Lisbeth <lizbeth1986.jm@gmail.com>
2018-03-06 21:55:24 +0000

    add browsersyn and env

--- gulpfile.js
+++ gulpfile.js
@@ -1,40 +1,77 @@
-var gulp = require('gulp');
-var pug = require('gulp-pug');
-var babel = require('gulp-babel');
-var concat = require('gulp-concat');
-var uglify = require('gulp-uglify');
-var rename = require('gulp-rename');
-var cleanCSS = require('gulp-clean-css');
-var del = require('del');
-var imagemin = require('gulp-imagemin');
+'use strict';
 
+var gulp = require('gulp'),
+    pug = require('gulp-pug'),
+    babel = require('gulp-babel'),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify'),
+    rename = require('gulp-rename'),
+    cleanCSS = require('gulp-clean-css'),
+    del = require('del'),
+    imagemin = require('gulp-imagemin'),
+    gulpif = require('gulp-if'),
+    nodemon = require('gulp-nodemon'),
+    browserSync = require('browser-sync').create();
+
+var outputDir; 
+var env = process.env.NODE_ENV || 'development';
+
+if (env === 'development') {
+  outputDir = 'builds/development/';
+} else {
+  outputDir = 'builds/production/'
+}
 var paths = {
+  dist_files: outputDir + '/**/*.*',
   html: {
     src: 'src/*.html',
-    dest: 'assets/'
+    dest: outputDir
   },
   styles: {
     src: 'src/styles/**/*.css',
-    dest: 'assets/styles/'
+    dest: outputDir + 'styles/'
   },
   scripts: {
     src: 'src/scripts/**/*.js',
-    dest: 'assets/scripts/'
+    dest: outputDir + 'scripts/'
   },
   images: {
     src: 'src/images/**/*.{jpg,jpeg,png}',
-    dest: 'assets/images/'
+    dest: outputDir +'images/'
+  },
+  fonts: {
+    src: 'src/fonts/**/*.{ttf,woff, woff2,eot,svg}',
+    dest: outputDir + 'fonts/'
   }
 };
 
+var plugins = {
+			browserSync: {
+	    proxy: "localhost:3000",
+	    port: 5000, 
+	    files: [ 
+	      paths.dist_files 
+	    ],
+	    browser: 'google chrome',
+	    notify: true,
+	    open: true
+    },
+      nodemon: {
+      script: 'index.js',
+      ignore: [
+        'node_modules/'
+      ]
+    }
+}
+
 function clean() {
-  return del([ 'assets' ]);
+  return del([ outputDir ]);
 }
 
 function html() {
   return gulp.src(paths.html.src)
     .pipe(pug())
-    .pipe(gulp.dest(paths.html.dest))
+    .pipe(gulp.dest(paths.html.dest));
 }
 function styles() {
   return gulp.src(paths.styles.src)
@@ -60,11 +97,33 @@ function images() {
     .pipe(gulp.dest(paths.images.dest));
 }
 
+function fonts() {
+  return gulp.src(paths.fonts.src, { read: false })
+  .pipe(gulp.dest(paths.fonts.dest));
+}
+
+function server(cb) {
+  var called = false;
+  return nodemon(plugins.nodemon)
+  .on('start', function() {
+    if(!called) {
+      called = true;
+      cb();
+    }
+  });
+}
+
+function browserSyncInit(done) {
+  browserSync.init(plugins.browserSync);
+  done();
+}
 function watch() {
   gulp.watch(paths.html.src, html)
   gulp.watch(paths.scripts.src, scripts);
   gulp.watch(paths.styles.src, styles);
   gulp.watch(paths.images.src, images);
+  gulp.watch(paths.fonts.src, fonts);
+  browserSync.reload();
 }
 
 exports.clean = clean;
@@ -73,7 +132,17 @@ exports.styles = styles;
 exports.scripts = scripts;
 exports.images = images;
 exports.watch = watch;
+exports.fonts = fonts;
+exports.server = server;
+exports.browserSyncInit = browserSyncInit;
 
-var build = gulp.series(clean, gulp.parallel(html, styles, scripts, images));
-
-gulp.task('build', build);
+//DEVELOPER
+var dev = gulp.series(gulp.parallel(html, styles, scripts, images, fonts));
+// PRODUCTION 
+var prod = gulp.series(clean, gulp.parallel(html, styles, scripts, images, fonts));
+//DEFAULT 
+var def = gulp.series(clean, dev, server, gulp.parallel(watch, browserSyncInit));
+//WATCH
+gulp.task('dev', dev);
+gulp.task('prod', prod);
+gulp.task('default', def)
